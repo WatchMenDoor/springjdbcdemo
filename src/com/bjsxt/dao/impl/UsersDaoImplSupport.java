b2cabb437abf95d38f55c5c893148e1d4fbf0fef
WatchMenDoor <13218857038@163.com>
2020-10-08 21:57:18 +0900

    接口实现类通过继承JdbcDaoSupport，其内部含有jdbctemplate属性，就只需要在配置文件中修改注入方式即可。

--- src/com/bjsxt/dao/impl/UsersDaoImplSupport.java
+++ src/com/bjsxt/dao/impl/UsersDaoImplSupport.java
@@ -1,18 +1,37 @@
 package com.bjsxt.dao.impl;
 
 import com.bjsxt.dao.UsersDao;
-import com.bjsxt.dao.UsersDao2;
 import com.bjsxt.pojo.Users;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import java.util.List;
 
-public class UsersDaoImplSupport extends JdbcDaoSupport implements UsersDao2 {
+public class UsersDaoImplSupport extends JdbcDaoSupport implements UsersDao {
+        @Override
+        public int insertUsers(Users users) {
+                return 0;
+        }
+
+        @Override
+        public int[] batchInsertUsers(List<Users> users) {
+                return new int[0];
+        }
+
+        @Override
+        public Users selectUsersById(int userid) {
+                return null;
+        }
+
+        @Override
+        public List<Users> selectUsersByName(String username) {
+                return null;
+        }
+
         @Override
         public List<Users> selectUsersByName2(String username) {
-        String sql = "select * from users where username = ?";
-        Object[] param = new Object[]{username};
-        return this.getJdbcTemplate().query(sql,param,new BeanPropertyRowMapper<>(Users.class));
+                String sql = "select * from users where username = ?";
+                Object[] param = new Object[]{username};
+                return this.getJdbcTemplate().query(sql,param,new BeanPropertyRowMapper<>(Users.class));
         }
 }
